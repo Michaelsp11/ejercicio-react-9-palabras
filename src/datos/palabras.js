37f282c9a29d462c6acd69bf762a205577e21776
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-06-18 20:24:24 +0200

    Array de palabras creado

---
+++ src/datos/palabras.js
@@ -0,0 +1,124 @@
+export const palabras = [
+  {
+    id: 0,
+    texto: "me",
+    isLenguaje: false,
+  },
+  {
+    id: 1,
+    texto: "JavaScript",
+    isLenguaje: true,
+  },
+  {
+    id: 2,
+    texto: "el",
+    isLenguaje: false,
+  },
+  {
+    id: 3,
+    texto: "la",
+    isLenguaje: false,
+  },
+  {
+    id: 4,
+    texto: "gusta",
+    isLenguaje: false,
+  },
+  {
+    id: 5,
+    texto: "odio",
+    isLenguaje: false,
+  },
+
+  {
+    id: 6,
+    texto: "no",
+    isLenguaje: false,
+  },
+  {
+    id: 7,
+    texto: "muy",
+    isLenguaje: false,
+  },
+
+  {
+    id: 8,
+    texto: "y",
+    isLenguaje: false,
+  },
+  {
+    id: 9,
+    texto: "Java",
+    isLenguaje: true,
+  },
+  {
+    id: 10,
+    texto: "mucho",
+    isLenguaje: false,
+  },
+  {
+    id: 11,
+    texto: "de",
+    isLenguaje: false,
+  },
+  {
+    id: 12,
+    texto: "poco",
+    isLenguaje: false,
+  },
+  {
+    id: 13,
+    texto: "programador",
+    isLenguaje: false,
+  },
+  {
+    id: 14,
+    texto: "amigo",
+    isLenguaje: false,
+  },
+  {
+    id: 15,
+    texto: "programadora",
+    isLenguaje: false,
+  },
+  {
+    id: 16,
+    texto: "amiga",
+    isLenguaje: false,
+  },
+  {
+    id: 17,
+    texto: "soy",
+    isLenguaje: false,
+  },
+  {
+    id: 18,
+    texto: "también",
+    isLenguaje: false,
+  },
+  {
+    id: 19,
+    texto: "pero",
+    isLenguaje: false,
+  },
+  {
+    id: 20,
+    texto: "gustan",
+    isLenguaje: false,
+  },
+  {
+    id: 21,
+    texto: "gatos",
+    isLenguaje: false,
+  },
+  {
+    id: 22,
+    texto: "los",
+    isLenguaje: false,
+  },
+  {
+    id: 23,
+    texto: "perros",
+    isLenguaje: false,
+  },
+];
