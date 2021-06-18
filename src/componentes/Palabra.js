065e36e8c75e1ef6f8b18973505c48c214c7ef92
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-06-18 21:36:56 +0200

    Primera palabra de la derecha, sea cual sea, siempre tiene que tener la primera letra en mayúscula.

--- src/componentes/Palabra.js
+++ src/componentes/Palabra.js
@@ -1,10 +1,17 @@
 export const Palabra = (props) => {
   const { palabra, resultado, setResultado } = props;
+  const { texto } = palabra;
   const agregarPalabra = () => {
     setResultado([
       ...resultado,
-      { id: resultado.length + 1, texto: palabra.texto },
+      {
+        id: resultado.length + 1,
+        texto:
+          resultado.length === 0
+            ? texto.charAt(0).toUpperCase() + texto.slice(1, texto.length)
+            : texto,
+      },
     ]);
   };
-  return <li onClick={agregarPalabra}>{palabra.texto}</li>;
+  return <li onClick={agregarPalabra}>{texto}</li>;
 };
